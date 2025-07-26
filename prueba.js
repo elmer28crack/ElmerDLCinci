cced7c7554217596a3c9cbac3fbbc35fe01ab2b3
Elmer Diaz Lopez <elmernicolasdiazlopez@gmail.com>
2025-07-26 09:19:57 -0400

    se movieron a otra carpeta

--- prueba.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementById("caja").innerHTML = 'XDDDD';
-
-
--- variable.js
+++
@@ -1,6 +0,0 @@
-var NOMBRES = "1";
-var NOMBRES1 ="2";
-
-var datos = `Dato uno es ${NOMBRES} y los dato dos es ${NOMBRES1}`
-console.log(datos);
-
